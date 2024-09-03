b1066fa0288c2d156f2d1cd6c91b42fb5db2307e
iraxit <102823307+iraxit@users.noreply.github.com>
2024-09-03 17:46:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-let http = require ("http");
-let app = require("./app");
-let port = process.env.PORT || 5000;
-let server = http.createServer(app)
-server.listen(port,() => {
+let express = require("express");
+let bodyParser = require("body-parser");
+let dotenv = require("dotenv");
+let OpenAI =require("openai");
+const { env } = require("process");
+
+//Load the environment
+dotenv.config();
+
+
+//Create the api_end_point
+const app = express();
+app.use(bodyParser.json())
+app.get("/",async (req,res) => {
+  let content = await main();
+  res.send(content);
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port,()=> {
   console.log("Server is running on port ${port}")
 });
+
+
 
 
 //Sample node.js code
